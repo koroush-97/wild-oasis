9aa128803d402dbeb4a642ce95518296b6c67e4b
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-19 22:06:03 +0330

    create heading component and send H1 styled component on it

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import Globalstyles from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
 const StyleApp = styled.main`
   background-color: orangered;
   padding: 20px;
