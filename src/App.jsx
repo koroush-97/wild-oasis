c640b3d14087bdc3a73ce3ad84141c3cf0a00d24
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-19 21:12:20 +0330

    add h1 ((hard code)) style in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
 import Globalstyles from "./styles/GlobalStyle";
 import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
 
 function App() {
   return (
