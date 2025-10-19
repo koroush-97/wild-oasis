b06208113e716ffab2e2afe15b9b4fbf5f33973c
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-19 22:22:10 +0330

    write condition for headings

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ function App() {
     <>
       <Globalstyles />
       <StyleApp>
-        <Heading> the Wild Oasis </Heading>;
+        <Heading type="h1"> the Wild Oasis </Heading>
+        <Heading type="h2"> Check in and out </Heading>
         <Button onClick={() => alert("Chick in")}> Chick in </Button>
         <Button onClick={() => alert("Chick Out")}> Chick out </Button>
+        <Heading type="h3"> Form </Heading>
         <Input placeholder="Number of guests" />
         <Input placeholder="Number of guests" />
       </StyleApp>
