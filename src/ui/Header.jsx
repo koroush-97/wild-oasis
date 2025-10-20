9af0fad1f0299269e972df084ccf07e3e9de4469
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-20 19:29:06 +0330

    add styled component for Header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: orangered;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
 function Header() {
-  return <header>Header</header>;
+  return <StyledHeader>Header</StyledHeader>;
 }
 
 export default Header;
