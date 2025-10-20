75e9c1a28ce19768ea0c487f099ad465a079e9fc
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-20 11:30:09 +0330

    add  GlobalStyles on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+// Global styles
+import GlobalStyles from "./styles/GlobalStyle";
+// React router dep
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// @pages
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -10,19 +14,22 @@ import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate replace to="dashboard" />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="users" element={<Users />} />
-        <Route path="setting" element={<Settings />} />
-        <Route path="account" element={<Account />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="setting" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
