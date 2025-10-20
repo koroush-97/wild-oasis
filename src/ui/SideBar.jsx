0f91b8bb24d1ae5437cf52e39ebf8d614e0b42dc
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-20 19:33:04 +0330

    define styled component for Sidebar

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: blue;
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+
 function SideBar() {
-  return <aside>SideBar</aside>;
+  return <StyledSidebar>SideBar</StyledSidebar>;
 }
 
 export default SideBar;
