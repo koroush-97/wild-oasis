9717f764802772e405d19a7fe7cc319da2218a14
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-24 11:20:06 +0330

    define a component for cabins table

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,6 +28,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -11,10 +11,6 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
-      <img
-        src="https://vdclfcjrdmwrsxpeihhw.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-        alt="cabin-image"
-      />
     </Row>
   );
 }
