3e6db3610a1f216c2e2984ceda395a5fc50c5608
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-24 08:09:40 +0330

    complete getCabins by adding error handling and returning data

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -2,4 +2,11 @@ import supabase from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins coild not be loaded");
+  }
+
+  return data;
 }
