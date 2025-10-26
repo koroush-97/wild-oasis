95afb98b110922d6cc9074f5b18a4db7e9037af1
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-10-26 19:40:47 +0330

    add deleteCabin function to remove cabins from database

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,10 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { error } = await supabase
+    .from("cabins")
+    .delete()
+    .eq("some_column", "someValue");
+}
