83a47c5615eb9536028c46c3c65857d6d6234285
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-07 11:07:28 +0330

    generate unique and valid image name for cabin upload

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,12 +12,23 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replace("/", "");
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // https://vdclfcjrdmwrsxpeihhw.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin }]);
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be created");
   }
+
+  // 2. Upload image
 
   return data;
 }
