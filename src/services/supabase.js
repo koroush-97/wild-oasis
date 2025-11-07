3f14c196bb45a7cc26cf44e4b0987e726b7445b4
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-07 12:12:23 +0330

    fix upload images function and it work nice

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vdclfcjrdmwrsxpeihhw.supabase.co";
+export const supabaseUrl = "https://vdclfcjrdmwrsxpeihhw.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkY2xmY2pyZG13cnN4cGVpaGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMDkwMDksImV4cCI6MjA3NjU4NTAwOX0.qP4--bi7vZNzEqxDRl8y364maVF9Gisu6T0OulbIYno";
 const supabase = createClient(supabaseUrl, supabaseKey);
