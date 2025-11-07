2283449e23c7e883aa3d83a0f59ed1d1bda07a08
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-07 16:28:25 +0330

    create custom useCreateCabin hook with mutation and query invalidation logic

--- src/features/cabins/useCreateCabin.js
+++ src/features/cabins/useCreateCabin.js
@@ -1,11 +1,19 @@
-const queryClient = useQueryClient();
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
-const { mutate, isPending: isCreating } = useMutation({
-  mutationFn: createCabin,
-  onSuccess: () => {
-    toast.success("New cabin successfully created");
-    queryClient.invalidateQueries({ queryKey: ["cabin"] });
-    reset();
-  },
-  onError: (err) => toast.error(err.message),
-});
+export function useCreateCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabin"] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isCreating, createCabin };
+}
