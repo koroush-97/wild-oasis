a5429fff76362b03dd1d6d12333e1de82d4c3976
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-08 11:06:26 +0330

    use useCabins hook in CabinTable for data fetching

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -29,6 +30,8 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
+  const { isLoading, cabins } = useCabins();
+
   if (isLoading) return <Spinner />;
 
   return (
