56b449f203e2a1dd634d308651825cbbfd246dc5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-08 16:23:13 +0330

    populate settings form with data from useSettings hook

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,23 +4,36 @@ import Input from "../../ui/Input";
 import { useSettings } from "./useSetting";
 
 function UpdateSettingsForm() {
-  const { isLoading, settings:{
-    minBookingLength,
-    maxBookingLengh,
-  } } = useSettings();
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLengh,
+      maxGuestsPerBooking,
+      BreakfastPrice,
+    },
+  } = useSettings();
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights"  defaultValue={} />
+        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={} />
+        <Input type="number" id="max-nights" defaultValue={maxBookingLengh} />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" defaultValue={} />
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+        />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" defaultValue={} />
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={BreakfastPrice}
+        />
       </FormRow>
     </Form>
   );
