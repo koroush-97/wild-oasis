83b865b6e853857b0a7c9bac125bc16826864ad6
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-08 16:13:17 +0330

    implement UpdateSettingsForm layout with form rows and inputs

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,26 @@
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSettings } from "./useSetting";
 
 function UpdateSettingsForm() {
+  const { isLoading, settings:{
+    minBookingLength,
+    maxBookingLengh,
+  } } = useSettings();
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" />
+        <Input type="number" id="min-nights"  defaultValue={} />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" />
+        <Input type="number" id="max-nights" defaultValue={} />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" />
+        <Input type="number" id="max-guests" defaultValue={} />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" />
+        <Input type="number" id="breakfast-price" defaultValue={} />
       </FormRow>
     </Form>
   );
