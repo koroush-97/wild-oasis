b431e4c174eeeca1404f1cc4e43cacd24ae2f836
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-08 16:38:59 +0330

    fix typo and clean up useUpdateSetting hook implementation

--- src/features/settings/useUpdateSetting.js
+++ src/features/settings/useUpdateSetting.js
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createEditCabin } from "../../services/apiCabins";
+
 import toast from "react-hot-toast";
 import { updateSetting as updateSettingApi } from "../../services/apiSettings";
 
 export function useUpdateSetting() {
   const queryClient = useQueryClient();
 
-  const { mutate: updateSetting, isPending: isEditing } = useMutation({
+  const { mutate: updateSetting, isPending: isUpdateing } = useMutation({
     mutationFn: updateSettingApi,
     onSuccess: () => {
       toast.success("Settings successfully edited");
@@ -15,5 +15,5 @@ export function useUpdateSetting() {
     onError: (err) => toast.error(err.message),
   });
 
-  return { isEditing, updateSetting };
+  return { isUpdateing, updateSetting };
 }
