988ec7393b3d61efc4f78305ce09cc58ea8a49a8
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-10 10:52:49 +0330

    Add cabins array with id, name, and room count for hotel cabins

---
+++ src/features/cabins/CabinsMock.js
@@ -0,0 +1,17 @@
+const cabins = [
+  {
+    id: 1,
+    name: "Cabin Deluxe",
+    rooms: 2
+  },
+  {
+    id: 2,
+    name: "Sea View Cabin",
+    rooms: 3
+  },
+  {
+    id: 3,
+    name: "Mountain Retreat",
+    rooms: 4
+  }
+];
