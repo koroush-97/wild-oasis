a895d201d995278a5da4ed8bc990cefde5bb1515
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-11 09:55:51 +0330

    impelement AddCabin

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,20 +4,32 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
-export default function AddCabin() {
-  const [IsOpenModal, setIsOpenModal] = useState(false);
+// function AddCabin() {
+//   const [IsOpenModal, setIsOpenModal] = useState(false);
 
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         {IsOpenModal && "close"}
+//         {!IsOpenModal && " Add new cabin"}
+//       </Button>
+//       {IsOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
+
+function AddCabin() {
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        {IsOpenModal && "close"}
-        {!IsOpenModal && " Add new cabin"}
-      </Button>
-      {IsOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Window>
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+export default AddCabin;
