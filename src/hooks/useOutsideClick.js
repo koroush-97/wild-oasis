2f522bc980f407a9f135db574d6eb26e8a54ddf2
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-11 16:50:59 +0330

    fix bug of stopPropegation

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -13,7 +13,7 @@ export function useOutsideClick(handler, listenCapturing = true) {
 
       document.addEventListener("click", handleClick, listenCapturing);
 
-      return () => document.removeEventListener("click", listenCapturing);
+      return () => document.removeEventListener("click", handleClick);
     },
     [handler, listenCapturing]
   );
