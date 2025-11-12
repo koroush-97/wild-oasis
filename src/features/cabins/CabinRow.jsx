75f45a2a9dc279e9e484bd2e9929319eec788c30
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-13 01:44:25 +0330

    Replace previous menu buttons with integrated Menus.Button inside Modal.Open for Edit and Delete

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -94,25 +94,20 @@ function CabinRow({ cabin }) {
               <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
                 Duplicate
               </Menus.Button>
-              <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+
+              <Modal.Open opens="edit">
+                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+              </Modal.Open>
+
+              <Modal.Open opens="delete">
+                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+              </Modal.Open>
             </Menus.List>
 
-            <Modal.Open opens="edit">
-              <button>
-                {" "}
-                <HiPencil />{" "}
-              </button>
-            </Modal.Open>
             <Modal.Window name="edit">
               <CreateCabinForm cabinToEdit={cabin} />
             </Modal.Window>
 
-            <Modal.Open opens="delete">
-              <button>
-                <HiTrash />
-              </button>
-            </Modal.Open>
             <Modal.Window name="delete">
               <ConfirmDelete
                 resourceName="cabins"
