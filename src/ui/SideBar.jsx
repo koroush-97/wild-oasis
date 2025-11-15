bd864bc33a72db00d312780724e43cb3cb67da7d
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-15 21:52:51 +0330

    impelement booking logic and set common name between post cabins and supabase

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 // @ components
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -19,6 +20,8 @@ function SideBar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyledSidebar>
   );
 }
