8bdccfc65ee788c8c5c64046e5a16608ade2f1f8
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-15 09:19:03 +0330

    impelement CabinTableOpreations component

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,10 @@
+import { TbTableOptions } from "react-icons/tb";
+import Filter from "../../ui/Filter";
+
+export default function CabinTableOperations() {
+  return (
+    <TbTableOptions>
+      <Filter />
+    </TbTableOptions>
+  );
+}
