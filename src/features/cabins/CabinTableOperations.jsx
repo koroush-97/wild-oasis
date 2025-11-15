37a5d1eda7b9c331d480d8c1120311da4ed30536
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-15 11:53:51 +0330

    add full sorting options for name, price and capacit

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -14,8 +14,16 @@ function CabinTableOperations() {
         ]}
       />
 
-      <SortBy options={[{ value: "name-asc", label: "Sort by name (A-Z)" }]} />
-      <SortBy options={[{ value: "name-desc", label: "Sort by name (Z-A)" }]} />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by price (low first)" },
+          { value: "regularPrice-desc", label: "Sort by price (high first)" },
+          { value: "maxCapacity-asc", label: "Sort by capacity (low first)" },
+          { value: "maxCapacity-desc", label: "Sort by capacity (high first)" },
+        ]}
+      />
     </TableOperations>
   );
 }
