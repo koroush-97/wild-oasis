b7f2e33627bd84d23f6226ae4bfa1ccc8147a4a3
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-16 11:40:36 +0330

    add pagination buttons with next/previous controls

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,3 +1,4 @@
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import styled from "styled-components";
 
 const StyledPagination = styled.div`
@@ -62,6 +63,16 @@ function Pagination() {
       <p>
         Showing <span>1</span> to <span>10</span> of <span> 23 </span> results
       </p>
+
+      <Buttons>
+        <PaginationButton>
+          <HiChevronLeft /> <span>Previous</span>
+        </PaginationButton>
+        <PaginationButton>
+          <span>Next</span>
+          <HiChevronRight />
+        </PaginationButton>
+      </Buttons>
     </StyledPagination>
   );
 }
