06682a41344b5cf2850f8de35e647f44e1faef67
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-18 17:19:34 +0330

    style: add Fulpage component style

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,8 +1,20 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-gery-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function ProtectedRoute({ children }) {
   // 1. Load the authenticated user
-
+  const { user, isLoading } = useUser();
   // 2.While loading , show a spinner
-
+  if (isLoading) return <Spinner />;
   // 3.If there is NO authenticated
 
   // 4. If there Is a user , render the app
