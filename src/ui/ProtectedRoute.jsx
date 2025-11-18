f52973e350af8651a93cac6924c895d77900e470
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-18 18:04:15 +0330

    feat(auth): enhance ProtectedRoute with redirect to login when unauthenticated

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const FullPage = styled.div`
   height: 100vh;
@@ -11,16 +13,26 @@ const FullPage = styled.div`
 `;
 
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
   // 1. Load the authenticated user
-  const { user, isLoading } = useUser();
-  // 2.While loading , show a spinner
+  const { isAuthenticated, isLoading } = useUser();
+
+  // 2.If there is NO authenticated
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3.While loading , show a spinner
+
   if (isLoading)
     return (
       <FullPage>
         <Spinner />;
       </FullPage>
     );
-  // 3.If there is NO authenticated
 
   // 4. If there Is a user , render the app
 
