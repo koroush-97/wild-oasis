9c347d5111039a30bb3ea1439b9113e7b234d71b
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-19 22:47:42 +0330

    feat( HeaderMenu  ) : impelement

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import Logout from "../features/authentication/Logout";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={} > 
+            <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
