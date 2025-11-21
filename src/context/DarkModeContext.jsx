31179d5c90b226cbded378c8b4f7c20dfe9579c5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-21 16:56:07 +0330

    feat: add DarkModeToggle component with basic icon button and logic

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState();
+}
