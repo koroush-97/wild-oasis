7505469866ce57bffa97fa6001b8761c81306be6
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-21 17:03:48 +0330

    feat(DarkModeProvider) Impelement and add logic of toggle btn for dark mode and light mode

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,5 +4,15 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState();
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
 }
