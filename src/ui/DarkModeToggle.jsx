ad96247fb61fa24d99e616b8add70cadbac9e7c9
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-22 02:43:45 +0330

    feat: import sun icon for dark mode toggle

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,4 +1,5 @@
-import { HiOutlineMoon } from "react-icons/hi";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
+
 import ButtonIcon from "./ButtonIcon";
 import { useDarkMode } from "../context/DarkModeContext";
 
