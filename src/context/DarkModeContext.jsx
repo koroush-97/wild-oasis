f895b18de567c7a58f3a2d9ab9b0472a9b0ab45f
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-22 03:00:04 +0330

    fix(dark-mode): correctly add dependency array to useEffect and toggle classes

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -6,15 +6,16 @@ const DarkModeContext = createContext();
 function DarkModeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
 
-  useEffect(
-    function () {
-      if (isDarkMode) {
-        document.documentElement.classList.add("dark-mode");
-      }
+  useEffect(function () {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
       document.documentElement.classList.remove("light-mode");
-    },
-    [isDarkMode]
-  );
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+    [isDarkMode];
+  });
 
   function toggleDarkMode() {
     setIsDarkMode((isDark) => !isDark);
