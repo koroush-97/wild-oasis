3a4a4bd183954c405ec6e30b428e8390d1d7b0b0
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-22 02:56:51 +0330

    feat: apply dark-mode class to document root based on isDarkMode state

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,10 +1,20 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+      }
+      document.documentElement.classList.remove("light-mode");
+    },
+    [isDarkMode]
+  );
 
   function toggleDarkMode() {
     setIsDarkMode((isDark) => !isDark);
