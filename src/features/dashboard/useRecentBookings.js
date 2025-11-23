30c2c1992b70036cbfacc2fdf05aaa24c35b0b13
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-23 09:46:53 +0330

    feat(bookings): add useRecentBookings hook with dynamic date range based on URL search params

--- src/features/dashboard/useRecentBookings.js
+++ src/features/dashboard/useRecentBookings.js
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
 import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiBookings";
 
 export function useRecentBookings() {
   const [searchParams] = useSearchParams();
@@ -6,4 +9,10 @@ export function useRecentBookings() {
   const numDays = !searchParams.get("last")
     ? 7
     : Number(searchParams.get("last"));
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: ["bookings"],
+  });
 }
