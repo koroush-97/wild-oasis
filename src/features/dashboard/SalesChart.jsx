a12783df026038aa3d4b8c1639c0df1ac25c38f9
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-23 13:13:39 +0330

    add SalesChart components and logic of it

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -57,3 +58,11 @@ const colors = isDarkMode
       text: "#374151",
       background: "#fff",
     };
+
+export default function SalesChart() {
+  return (
+    <StyledSalesChart>
+      <Heading as="h2">Sales</Heading>
+    </StyledSalesChart>
+  );
+}
